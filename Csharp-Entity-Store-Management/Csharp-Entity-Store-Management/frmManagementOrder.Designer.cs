daa9242e48be17a84d5028039fa97fa14e528eaa
= <=>
2021-04-24 09:49:20 +0700

    fix Order Form

--- Csharp-Entity-Store-Management/Csharp-Entity-Store-Management/frmManagementOrder.Designer.cs
+++ Csharp-Entity-Store-Management/Csharp-Entity-Store-Management/frmManagementOrder.Designer.cs
@@ -45,21 +45,19 @@ namespace Csharp_Entity_Store_Management
             // 
             this.dgvOrder.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
             this.dgvOrder.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dgvOrder.Location = new System.Drawing.Point(67, 284);
-            this.dgvOrder.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.dgvOrder.Location = new System.Drawing.Point(50, 155);
             this.dgvOrder.Name = "dgvOrder";
             this.dgvOrder.RowHeadersVisible = false;
             this.dgvOrder.RowHeadersWidth = 51;
-            this.dgvOrder.Size = new System.Drawing.Size(573, 236);
+            this.dgvOrder.Size = new System.Drawing.Size(430, 192);
             this.dgvOrder.TabIndex = 0;
             this.dgvOrder.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.cellClickItem);
             // 
             // btnShowAOrder
             // 
-            this.btnShowAOrder.Location = new System.Drawing.Point(905, 23);
-            this.btnShowAOrder.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnShowAOrder.Location = new System.Drawing.Point(576, 155);
             this.btnShowAOrder.Name = "btnShowAOrder";
-            this.btnShowAOrder.Size = new System.Drawing.Size(128, 28);
+            this.btnShowAOrder.Size = new System.Drawing.Size(96, 23);
             this.btnShowAOrder.TabIndex = 1;
             this.btnShowAOrder.Text = "Xem đơn hàng";
             this.btnShowAOrder.UseVisualStyleBackColor = true;
@@ -67,10 +65,9 @@ namespace Csharp_Entity_Store_Management
             // 
             // btnDeleteAOrder
             // 
-            this.btnDeleteAOrder.Location = new System.Drawing.Point(905, 492);
-            this.btnDeleteAOrder.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnDeleteAOrder.Location = new System.Drawing.Point(576, 252);
             this.btnDeleteAOrder.Name = "btnDeleteAOrder";
-            this.btnDeleteAOrder.Size = new System.Drawing.Size(128, 28);
+            this.btnDeleteAOrder.Size = new System.Drawing.Size(96, 23);
             this.btnDeleteAOrder.TabIndex = 2;
             this.btnDeleteAOrder.Text = "Xóa đơn hàng";
             this.btnDeleteAOrder.UseVisualStyleBackColor = true;
@@ -88,11 +85,9 @@ namespace Csharp_Entity_Store_Management
             this.OrderManagement.Controls.Add(this.btnShowAOrder);
             this.OrderManagement.Controls.Add(this.dgvOrder);
             this.OrderManagement.Controls.Add(this.btnDeleteAOrder);
-            this.OrderManagement.Location = new System.Drawing.Point(13, 13);
-            this.OrderManagement.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.OrderManagement.Location = new System.Drawing.Point(10, 11);
             this.OrderManagement.Name = "OrderManagement";
-            this.OrderManagement.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
-            this.OrderManagement.Size = new System.Drawing.Size(1041, 528);
+            this.OrderManagement.Size = new System.Drawing.Size(781, 429);
             this.OrderManagement.TabIndex = 3;
             this.OrderManagement.TabStop = false;
             this.OrderManagement.Text = "Quản lí đơn hàng";
@@ -100,50 +95,45 @@ namespace Csharp_Entity_Store_Management
             // lbUserID
             // 
             this.lbUserID.AutoSize = true;
-            this.lbUserID.Location = new System.Drawing.Point(344, 100);
-            this.lbUserID.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lbUserID.Location = new System.Drawing.Point(258, 81);
             this.lbUserID.Name = "lbUserID";
-            this.lbUserID.Size = new System.Drawing.Size(97, 17);
+            this.lbUserID.Size = new System.Drawing.Size(75, 13);
             this.lbUserID.TabIndex = 6;
             this.lbUserID.Text = "Mã nhân viên:";
             // 
             // lbCusID
             // 
             this.lbCusID.AutoSize = true;
-            this.lbCusID.Location = new System.Drawing.Point(77, 101);
-            this.lbCusID.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lbCusID.Location = new System.Drawing.Point(58, 82);
             this.lbCusID.Name = "lbCusID";
-            this.lbCusID.Size = new System.Drawing.Size(113, 17);
+            this.lbCusID.Size = new System.Drawing.Size(88, 13);
             this.lbCusID.TabIndex = 5;
             this.lbCusID.Text = "Mã khách hàng: ";
             // 
             // lbCreateAt
             // 
             this.lbCreateAt.AutoSize = true;
-            this.lbCreateAt.Location = new System.Drawing.Point(340, 52);
-            this.lbCreateAt.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lbCreateAt.Location = new System.Drawing.Point(255, 42);
             this.lbCreateAt.Name = "lbCreateAt";
-            this.lbCreateAt.Size = new System.Drawing.Size(68, 17);
+            this.lbCreateAt.Size = new System.Drawing.Size(52, 13);
             this.lbCreateAt.TabIndex = 4;
             this.lbCreateAt.Text = "Ngày lập:";
             // 
             // lbOrderID
             // 
             this.lbOrderID.AutoSize = true;
-            this.lbOrderID.Location = new System.Drawing.Point(77, 52);
-            this.lbOrderID.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lbOrderID.Location = new System.Drawing.Point(58, 42);
             this.lbOrderID.Name = "lbOrderID";
-            this.lbOrderID.Size = new System.Drawing.Size(91, 17);
+            this.lbOrderID.Size = new System.Drawing.Size(71, 13);
             this.lbOrderID.TabIndex = 3;
             this.lbOrderID.Text = "Mã hóa đơn: ";
             // 
             // frmManagementOrder
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1067, 554);
+            this.ClientSize = new System.Drawing.Size(800, 450);
             this.Controls.Add(this.OrderManagement);
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "frmManagementOrder";
             this.Text = "frmManagementOrder";
             this.Load += new System.EventHandler(this.frmManagementOrder_Load);
